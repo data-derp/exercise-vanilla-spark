47622993e62835f49030db3e6481b66875ba3c39
Syed <masroor.rizvi@gmail.com>
2023-04-09 21:47:10 +0530

    Added stateless word count

---
+++ src/09 - StreamingStatelessSocketWordCount.py
@@ -0,0 +1,51 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+import pyspark.sql.functions as f
+
+spark = SparkSession \
+    .builder \
+    .appName("StructuredNetworkWordCount") \
+    .getOrCreate()
+
+spark.sparkContext.setLogLevel("ERROR")
+
+# Create DataFrame representing the stream of input lines from connection to localhost:9999
+# This acts like the source of data
+lines = spark \
+    .readStream \
+    .format("socket") \
+    .option("host", "localhost") \
+    .option("port", 9999) \
+    .load()
+
+# Split the lines into words
+words = lines.select(
+   explode(
+       split(lines.value, " ")
+   ).alias("word")
+)
+
+print(words.printSchema())
+
+# Generate running word count
+wordCounts = words.withColumn('count', f.size(f.split(f.col('word'), ' ')))
+wordCounts.printSchema()
+
+
+#
+# # Keep the partitions as 3
+# spark.conf.set("spark.sql.shuffle.partitions", "3")
+#
+# # print('Before Setting -> ' + spark.conf.get("spark.sql.adaptive.enabled"))
+# spark.conf.set("spark.sql.adaptive.enabled", False)
+# print('Before Setting -> ' + spark.conf.get("spark.sql.adaptive.enabled"))
+
+# Start running the query that prints the running counts to the console
+query = wordCounts \
+    .writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .start()
+
+query.awaitTermination()
