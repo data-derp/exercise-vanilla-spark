3bc341181dcd0bace26a03959b3fdcd47d5383b2
Syed <syedalimasroor.r@thoughtworks.com>
2024-05-26 17:22:13 +0530

    Added the sliding time window code

---
+++ src/12.5 - StreamingSlidingWindow.py
@@ -0,0 +1,47 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+spark = SparkSession \
+    .builder \
+    .appName("Sliding Window Wordcount") \
+    .getOrCreate()
+
+spark.sparkContext.setLogLevel("ERROR")
+
+socketStreamDF = spark.readStream \
+    .format("socket") \
+    .option("host", "localhost") \
+    .option("port", 9999) \
+    .load()
+
+stocksDF = socketStreamDF.withColumn("value", split("value", ","))\
+     .withColumn("EventTime", to_timestamp(col("value")[0], "yyyy-MM-dd HH:mm:ss")) \
+     .withColumn("symbol", col("value")[1]).withColumn("price", col("value")[2].cast(DoubleType()))
+
+stocksDF.printSchema()
+
+# Group the data by window and word and compute the count of each group
+windowedWords = stocksDF\
+    .groupBy(window("EventTime", "1 minute", "30 seconds"), stocksDF.symbol)\
+    .agg(max("price").alias("maxPrice"))
+
+windowedWords.printSchema()
+
+# outputDF = windowedWords.select("window.start", "window.end", "Symbol", "TotalPrice")
+
+# outputDF.printSchema()
+
+# This is like writing the data to the sink, console in this case
+query = windowedWords \
+    .writeStream \
+    .outputMode("update") \
+    .format("console") \
+    .option('truncate', 'false') \
+    .start()
+
+query.awaitTermination()
+
+
+
+
